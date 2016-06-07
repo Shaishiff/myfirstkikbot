1817397124d9d6c27f6cee4c5c935d7911b7ec91
Shiff <shiff@Shiffs-MacBook-Air.local>
2016-06-07 23:46:54 +0300

    - changed base url

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ let Bot  = require('@kikinteractive/kik');
 let bot = new Bot({
     username: 'shaisbot',
     apiKey: '274f113b-2abd-44ec-9193-d49181a36a55',
-    baseUrl: 'https://beepboophq.com/proxy/56fc42b2ce9746f09b241e0c471914ec'
+    baseUrl: 'https://beepboophq.com/proxy/56fc42b2ce9746f09b241e0c471914ec/incoming'
 });
 
 bot.updateBotConfiguration();
@@ -17,6 +17,10 @@ bot.onTextMessage((message) => {
     console.log("Got message: " + message.body);
     message.reply(message.body);
 });
+
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
 
 // Set up your server and start listening
 let server = http
